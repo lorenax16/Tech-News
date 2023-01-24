2e7f025de64dde968295363e86b268a0ee181ee4
lorena olivares <lorena.olivares.perez16@gmail.com>
2023-01-24 14:18:23 -0300

    req 6 e 8

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,6 +1,13 @@
+from tech_news.database import search_news
+
+
 # Requisito 6
 def search_by_title(title):
-    """Seu código deve vir aqui"""
+    news = search_news({"title": {"$regex": title, "$options": "i"}})
+    result = []
+    for new in news:
+        result.append((new["title"], new["url"]))
+    return result
 
 
 # Requisito 7
@@ -10,7 +17,12 @@ def search_by_date(date):
 
 # Requisito 8
 def search_by_tag(tag):
-    """Seu código deve vir aqui"""
+    lista_resultado = []
+    nova_lista = search_news({"tags": {"$regex": tag, "$options": "i"}})
+    for nova in nova_lista:
+        list = (nova["title"], nova["url"])
+        lista_resultado.append(list)
+    return lista_resultado
 
 
 # Requisito 9

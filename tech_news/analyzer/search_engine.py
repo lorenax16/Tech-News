e1f231a7ed1f2984d44743843a8f17922539978e
lorena olivares <lorena.olivares.perez16@gmail.com>
2023-01-25 10:33:47 -0300

    req 7 e 9

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,7 +1,9 @@
 from tech_news.database import search_news
-
+from datetime import date as data
 
 # Requisito 6
+
+
 def search_by_title(title):
     news = search_news({"title": {"$regex": title, "$options": "i"}})
     result = []
@@ -12,7 +14,17 @@ def search_by_title(title):
 
 # Requisito 7
 def search_by_date(date):
-    """Seu código deve vir aqui"""
+    try:
+        formatar_data = data.fromisoformat(date).strftime("%d/%m/%Y")
+        nova_lista = search_news({"timestamp": formatar_data})
+        lista = []
+
+        for nova in nova_lista:
+            lista.append((nova["title"], nova["url"]))
+
+        return lista
+    except ValueError:
+        raise ValueError("Data inválida")
 
 
 # Requisito 8
@@ -27,4 +39,11 @@ def search_by_tag(tag):
 
 # Requisito 9
 def search_by_category(category):
-    """Seu código deve vir aqui"""
+    nova_lista = search_news(
+        {"category": {"$regex": category, "$options": "i"}}
+    )
+    lista = []
+
+    for nova in nova_lista:
+        lista.append((nova["title"], nova["url"]))
+    return lista
